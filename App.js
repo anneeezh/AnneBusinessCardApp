4966822ca96a75c5fc90a09b1ef42b8187a64b2c
anneeezh <sdzhaj@gmail.com>
2021-04-21 20:39:38 -0700

    main

--- App.js
+++ App.js
@@ -1,33 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Press me"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const ViewBoxesWithColorAndText = () => {
-  return (
-    <View
-      style={{
-        flexDirection: "row",
-        height: 100,
-        padding: 20
-      }}
-    >
-      <View style={{ backgroundColor: "blue", flex: 0.3 }} />
-      <View style={{ backgroundColor: "red", flex: 0.5 }} />
-      <Text>Hello World!</Text>
-    </View>
-  );
-};
-
-export default ViewBoxesWithColorAndText;
 
 const styles = StyleSheet.create({
   container: {
@@ -37,3 +24,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   });
+  
